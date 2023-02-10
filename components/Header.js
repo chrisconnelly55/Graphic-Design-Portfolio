95b69beafec2397855c5502cafff086d62b59a22
Chris <chrisconnelly55@gmail.com>
2023-02-10 09:01:25 +0000

    update navigation, create pages

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,12 @@
+import styles from "./Header.module.css";
+import Logo from "@components/Logo";
+import Navigation from "@components/Navigation";
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <div className={styles.header}>
+      <Logo title={title} />
+      <span className={styles.spacer} />
+      <Navigation />
+    </div>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '@styles/globals.css'
-
+import "@styles/globals.css";
+import Header from "@components/Header";
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header title="Connelly Designs" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default Application;
